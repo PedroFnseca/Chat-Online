c855aa2833561b8805513522f7573732b3a8c7ab
pedrofnseca <pedropereira.pp264@gmail.com>
2022-09-24 23:20:14 -0300

    Protegendo código js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,77 +1,78 @@
-var socket = io();
-
-var form = document.getElementById('form-message')
-var input = document.getElementById('message-input')
-var messagesContainer = document.getElementById('messages-container')
-var userName = localStorage.getItem('username')
-var id = localStorage.getItem('id')
-var scroll = document.querySelector('.scrollBar')
-var room = window.location.pathname.split('/')[1]
-input.focus()
-
-if (!userName){
-  window.location.href = '/'
-}
-
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    if (input.value) {
-
-        const date = new Date()
-        var minutes = date.getMinutes()
-        var hours = date.getHours()
-
-        if (minutes.toString().length == 1){
-            minutes = `0${minutes}`
+window.onload = () => {
+    var socket = io();
+    
+    var form = document.getElementById('form-message')
+    var input = document.getElementById('message-input')
+    var messagesContainer = document.getElementById('messages-container')
+    var userName = localStorage.getItem('username')
+    var id = localStorage.getItem('id')
+    var scroll = document.querySelector('.scrollBar')
+    var room = window.location.pathname.split('/')[1]
+    input.focus()
+    
+    if (!userName){
+    window.location.href = '/'
+    }
+    
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        if (input.value) {
+    
+            const date = new Date()
+            var minutes = date.getMinutes()
+            var hours = date.getHours()
+    
+            if (minutes.toString().length == 1){
+                minutes = `0${minutes}`
+            }
+            const time = `${hours}:${minutes}` 
+    
+            data = {
+                username: userName,
+                message: input.value,
+                time: time,
+                room: room,
+                id: id
+            }
+    
+            socket.emit('chatMessage', data)
+            input.value = '';
         }
-        const time = `${hours}:${minutes}` 
-
-        data = {
-            username: userName,
-            message: input.value,
-            time: time,
-            room: room,
-            id: id
-        }
-
-        socket.emit('chatMessage', data)
-        input.value = '';
-    }
-})
-
-// Entra na sala
-socket.emit('userConnected', room)
-
-socket.on('chatMessage', (message) => {
-    var li = document.createElement('li')
-    li.classList.add('message')
-    console.log(message)
-
-    if(message.id == id){
-        li.innerHTML = `<div class="my-message"><div class="minhas-mensagens"><span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
-    }else{
-        li.innerHTML = `<div class="others-message"><span class="message-username">${message.username}</span><div class="outras-mensagens">  <span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
-    }
-
-    messagesContainer.appendChild(li)
-
-    scroll.scrollTop = scroll.scrollHeight
-})
-
-socket.on('userConnected', (messages) =>{
-    messagesContainer.innerHTML = ''
-    messages.forEach(message => {
-    var li = document.createElement('li')
-    li.classList.add('message')
-
-    if(message.id == id){
-        li.innerHTML = `<div class="my-message"><div class="minhas-mensagens"><span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
-    }else{
-        li.innerHTML = `<div class="others-message"><span class="message-username">${message.username}</span><div class="outras-mensagens">  <span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
-    }
-
-    messagesContainer.appendChild(li)
     })
-
-    scroll.scrollTop = scroll.scrollHeight
-})
+    
+    // Entra na sala
+    socket.emit('userConnected', room)
+    
+    socket.on('chatMessage', (message) => {
+        var li = document.createElement('li')
+        li.classList.add('message')
+    
+        if(message.id == id){
+            li.innerHTML = `<div class="my-message"><div class="minhas-mensagens"><span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
+        }else{
+            li.innerHTML = `<div class="others-message"><span class="message-username">${message.username}</span><div class="outras-mensagens">  <span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
+        }
+    
+        messagesContainer.appendChild(li)
+    
+        scroll.scrollTop = scroll.scrollHeight
+    })
+    
+    socket.on('userConnected', (messages) =>{
+        messagesContainer.innerHTML = ''
+        messages.forEach(message => {
+        var li = document.createElement('li')
+        li.classList.add('message')
+    
+        if(message.id == id){
+            li.innerHTML = `<div class="my-message"><div class="minhas-mensagens"><span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
+        }else{
+            li.innerHTML = `<div class="others-message"><span class="message-username">${message.username}</span><div class="outras-mensagens">  <span class="message-content">${message.message}</span> <span class="message-time">${message.time}</span></div></div>`
+        }
+    
+        messagesContainer.appendChild(li)
+        })
+    
+        scroll.scrollTop = scroll.scrollHeight
+    })
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,51 +1,54 @@
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-var socket = io()
-const usernameInput = document.getElementById('username-input')
-const form = document.querySelector('form')
-const okBtn = document.getElementById('ok-btn')
-const spanOnline = document.getElementById('online')
-const room = document.getElementById('room-input')
-
-socket.on('online', (online) => {
-    spanOnline.innerHTML = `${online} online`
-})
-
-usernameInput.focus()
-
-// Gera um id Aleatório
-function generateId(){
-    const d = new Date()
-    return d.getDate() * d.getMilliseconds() + d.getSeconds() + d.getMinutes() 
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (usernameInput.value) {
-        const username = capitalizeFirstLetter(usernameInput.value)
-        
-        const name = localStorage.getItem('username')
-        const id = generateId()
-
-        // Caso o nome seja diferente ele altera o id
-        if(!name || name != username){
-            localStorage.setItem('username', username)
-            localStorage.setItem('id', id)
-        }
-
-        if(room.value == '') {
-            window.location.href = '/geral'
-        } else{
-            window.location.href = `/${room.value}`
-        }
-    } else{
-        alert('Digite um nome de usuário!')
+window.onload = () => {
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
-})
-
-function keyUpHandler(e) {
-    if (e.key === 'Enter') {
-        okBtn.click();
+    
+    var socket = io()
+    const usernameInput = document.getElementById('username-input')
+    const form = document.querySelector('form')
+    const okBtn = document.getElementById('ok-btn')
+    const spanOnline = document.getElementById('online')
+    const room = document.getElementById('room-input')
+    
+    socket.on('online', (online) => {
+        spanOnline.innerHTML = `${online} online`
+    })
+    
+    usernameInput.focus()
+    
+    // Gera um id Aleatório
+    function generateId(){
+        const d = new Date()
+        return d.getDate() * d.getMilliseconds() + d.getSeconds() + d.getMinutes() 
+    }
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (usernameInput.value) {
+            const username = capitalizeFirstLetter(usernameInput.value)
+            
+            const name = localStorage.getItem('username')
+            const id = generateId()
+    
+            // Caso o nome seja diferente ele altera o id
+            if(!name || name != username){
+                localStorage.setItem('username', username)
+                localStorage.setItem('id', id)
+            }
+    
+            if(room.value == '') {
+                window.location.href = '/geral'
+            } else{
+                window.location.href = `/${room.value}`
+            }
+        } else{
+            alert('Digite um nome de usuário!')
+        }
+    })
+    
+    function keyUpHandler(e) {
+        if (e.key === 'Enter') {
+            okBtn.click();
+        }
     }
 }
